208748d5b0f858695399765aeed3f54dc9ba60c7
Laxmanrajpurohit10 <laxman.rajpurohit@hx3.com>
2026-06-11 17:03:37 +0530

    completes the site 1sqmm

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ function App() {
       <Route path="/product/1-5-blue" element={<ProductPage />} />
       <Route path="/product/1-5-green" element={<ProductPage />} />
       <Route path="/product/1-0-red" element={<ProductPage1sqmm />} />
+      <Route path="/product/1-0-black" element={<ProductPage1sqmm />} />
+      <Route path="/product/1-0-yellow" element={<ProductPage1sqmm />} />
+      <Route path="/product/1-0-blue" element={<ProductPage1sqmm />} />
+      <Route path="/product/1-0-green" element={<ProductPage1sqmm />} />
     </Routes>
   );
 }
