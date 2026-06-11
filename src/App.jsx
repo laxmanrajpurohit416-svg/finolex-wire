f4c4b1847e974a2be6646d5909d2648ed230c8f1
Laxmanrajpurohit10 <laxman.rajpurohit@hx3.com>
2026-06-11 16:56:41 +0530

    Rest colors of size 1sqmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import ProductPage from "./Pages/ProductPage";
+import ProductPage1sqmm from "./Pages/ProductPage1sqmm";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/product/1-5-yellow" element={<ProductPage />} />
       <Route path="/product/1-5-blue" element={<ProductPage />} />
       <Route path="/product/1-5-green" element={<ProductPage />} />
+      <Route path="/product/1-0-red" element={<ProductPage1sqmm />} />
     </Routes>
   );
 }
